b69910864d3c51ca91248672f23b10628a9362d8
Bluejazzyrose <jasmine.fryefamily@gmail.com>
2025-10-20 18:51:28 -0500

    Added graphical bg to the display window using pygame and os

--- pygame_test.py
+++ pygame_test.py
@@ -8,20 +8,34 @@ Player race classes and maps are imported from separate files
 
 
 import pygame
+import os
 
 
 # Initialize Pygame
 pygame.init()
 
-# Screen Setup
-screen = pygame.display.set_mode((1200, 600), pygame.RESIZABLE)
+# Screen setup
+height = 600
+width = 1200
+screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption("Arete")
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+bg_file_name = "Graphics\Arete_BG_Clouds.png"
+bg_file = os.path.join(script_dir, bg_file_name)
+
+background_image = pygame.image.load(bg_file).convert_alpha()
+background_image = pygame.transform.scale(background_image, (width, height))
+screen.blit(background_image, (0, 0))
 
 # Color constants
 MIST = (243, 248, 255)
 AZURE = (83, 172, 197)
 COBALT = (21, 78, 114)
 NAVY = (14, 41, 72)
+
+# Menu setup
+
 
 # Game loop
 running = True
