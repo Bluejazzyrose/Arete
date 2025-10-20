049edeac06588db156454643c3823ce618104901
Bluejazzyrose <jasmine.fryefamily@gmail.com>
2025-10-20 18:36:18 -0500

    Working on PyGame display window

--- pygame_test.py
+++ pygame_test.py
@@ -1,13 +1,27 @@
+"""
+Author: Jasmine Frye
+Program: Arete game
+Vision: open-world MMO without leveling crawl, inspired by Greek mythology
+Prominent feature: unique death mechanics, not just 'respawn'
+Player race classes and maps are imported from separate files
+"""
+
+
 import pygame
+
 
 # Initialize Pygame
 pygame.init()
 
-# Set display mode to windowed
+# Screen Setup
 screen = pygame.display.set_mode((1200, 600), pygame.RESIZABLE)
-screen.fill((0, 0, 0)) # Fill the screen with black
-
 pygame.display.set_caption("Arete")
+
+# Color constants
+MIST = (243, 248, 255)
+AZURE = (83, 172, 197)
+COBALT = (21, 78, 114)
+NAVY = (14, 41, 72)
 
 # Game loop
 running = True
