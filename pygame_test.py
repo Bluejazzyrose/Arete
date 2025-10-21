64ef3e180e766281fc3ed8d01e4463bf687ca8ef
Bluejazzyrose <jasmine.fryefamily@gmail.com>
2025-10-20 22:30:58 -0500

    Updated graphical menu

--- pygame_test.py
+++ pygame_test.py
@@ -8,6 +8,9 @@ Player race classes and maps are imported from separate files
 
 
 import pygame
+import pygame_menu as pm
+from pygame_menu.themes import Theme
+from pygame_menu.baseimage import BaseImage, IMAGE_MODE_FILL
 import os
 
 
@@ -15,17 +18,17 @@ import os
 pygame.init()
 
 # Screen setup
-height = 600
-width = 1200
-screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+HEIGHT = 600
+WIDTH = 1200
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Arete")
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 bg_file_name = "Graphics\Arete_BG_Clouds.png"
-bg_file = os.path.join(script_dir, bg_file_name)
+bg_file_path = os.path.join(script_dir, bg_file_name)
 
-background_image = pygame.image.load(bg_file).convert_alpha()
-background_image = pygame.transform.scale(background_image, (width, height))
+background_image = pygame.image.load(bg_file_path).convert_alpha()
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 screen.blit(background_image, (0, 0))
 
 # Color constants
@@ -34,8 +37,33 @@ AZURE = (83, 172, 197)
 COBALT = (21, 78, 114)
 NAVY = (14, 41, 72)
 
-# Menu setup
+# Menu theme setup
+menu_bg = Theme()
+menu_bg.background_color = BaseImage(image_path = bg_file_path,
+                                     drawing_mode = IMAGE_MODE_FILL)
 
+# Menu setup
+menu = pm.Menu(title = "Main Menu",
+               width = WIDTH,
+               height = HEIGHT,
+               theme = menu_bg)
+
+# Settings button. If clicked, it takes to the settings menu
+menu.add.button(title="Play",
+                font_color=MIST,
+                background_color=NAVY,
+                background_inflate = (10, 10))
+
+# Dummy label to add some spacing between the settings button and exit button
+menu.add.label(title="")
+
+# Exit Button. If clicked, it closes the window
+menu.add.button(title="Exit", action=pm.events.EXIT,
+                font_color=MIST,
+                background_color=NAVY,
+                background_inflate = (10, 10))
+
+menu.mainloop(screen)
 
 # Game loop
 running = True
